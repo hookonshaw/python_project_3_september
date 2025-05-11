ab98064ece4970111de3b00d854a5b35714aa146
CerN04 <danyabadamschin@mail.ru>
2025-05-11 21:15:38 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,16 @@
 import sqlite3
+from datetime import datetime
+
+from hash_password import hash_password
 from fastapi import FastAPI, Depends, HTTPException, Response, Request
 from pydantic import BaseModel
+from typing import Optional
+from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
-from db_connect import Base, engine, get_db
+from db_connect import Base, engine, get_db, get_admin_id
 from fastapi.staticfiles import StaticFiles
 from authx import AuthX,AuthXConfig
-
-from sqlalchemy.orm import Session
-from models import Admin
-import bcrypt
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
@@ -16,9 +18,17 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="static")
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8000"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
-async def read_root():
-    return RedirectResponse(url="/static/index.html")
+async def main_page():
+    return RedirectResponse(url="/static/index_a_mere_mortal.html")
 
 Base.metadata.create_all(bind=engine)
 
@@ -40,30 +50,35 @@ class AdminsLoginSchema(BaseModel):
 async def login(creds: AdminsLoginSchema, response: Response):
     connection = sqlite3.connect('admins.db')
     cursor = connection.cursor()
-    inp_username, inp_password = creds.username, creds.password
+    inp_username, inp_password = creds.username, hash_password(str(creds.password))
     cursor.execute("SELECT * FROM admins WHERE name=? AND password_hash=?", (inp_username, inp_password))
     result = cursor.fetchone()
     if result:
-        print('Вход выполнен успешно!')
+        # print('Вход выполнен успешно!')
+        # print('Хэш-пароль: ', inp_password)
         token = security.create_access_token(uid=get_admin_id(inp_username, inp_password))
-        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+        # print("Токен - ", token)
+        # response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+        response.set_cookie(
+                    key="access_token", #config.JWT_ACCESS_COOKIE_NAME,
+                    value=token,
+                    httponly=True,
+                    secure=False,  # Для теста False (в продакшене True)
+                    samesite="lax",
+                    path="/"  
+                )
+        # print("Заголовки ответа:")
+        # print(response.headers)
         connection.close()
+        RedirectResponse(url="/static/index_admin.html", status_code=302)
         return {"access_token": token}
     raise HTTPException(status_code=401, detail="Неверный логин или пароль")
 
 
-@app.get("/protected", dependencies=[Depends(security.access_token_required)])
-async def protected():
-    return {"message": "Пользователь авторизован!"}
-
-
-def get_admin_id(log, password):
-    with sqlite3.connect('admins.db') as conn:
-        cursor = conn.cursor()
-        cursor.execute("SELECT id FROM admins WHERE name = ? AND password_hash = ?", (log, password))
-        result = cursor.fetchone()
-        return str(result[0]) if result else None
-
-
-
-
+@app.get("/protected")
+async def protected(request: Request, user=Depends(security.access_token_required)):
+    print("User in protected:", user)
+    if user:
+        return RedirectResponse(url="/static/index_admin.html")
+    else:
+        return RedirectResponse(url="/static/index_a_merge_mortal.html")
