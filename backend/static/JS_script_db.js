080f97dcb7cdb9766f1be585f635750e0223b4db
CerN04 <danyabadamschin@mail.ru>
2025-05-11 21:16:22 +0300

    Update JS_script_db.js

--- backend/static/JS_script_db.js
+++ backend/static/JS_script_db.js
@@ -1,28 +1,30 @@
 document.getElementById('loginForm').addEventListener('submit', async (e) => {
     e.preventDefault();
-    
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-    console.log('Отправляем:', { username, password }); // Для отладки в консоли браузера
     const messageDiv = document.getElementById('message');
-    
+
     try {
         const response = await fetch('/login', {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ username, password }) // JSON
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ username, password }),
+            credentials: 'include' // Передаем куки
         });
-        
-        if (response.ok) {
+
+        // Проверка редиректа
+        if (response.redirected) {
+            window.location.href = response.url;
+        } else if (response.status === 200) { // Если сервер возвращает JSON
             const data = await response.json();
-            localStorage.setItem('access_token', data.access_token); // Сохранение токена в localStorage
-            window.location.href = 'index.html';             // Перенаправление на главную страницу
-        } else {
+            localStorage.setItem('access_token', data.access_token);
+            window.location.href = '/static/index_admin.html';
+        } else if (response.status === 401) {
             messageDiv.textContent = 'Неверный логин или пароль';
+        } else {
+            messageDiv.textContent = 'Неизвестная ошибка';
         }
     } catch (error) {
-        messageDiv.textContent = 'Ошибка при входе';
+        messageDiv.textContent = 'Ошибка при отправке данных';
     }
 });
